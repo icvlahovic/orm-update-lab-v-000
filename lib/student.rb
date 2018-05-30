271435efd9741db7b7d510e979be58c5491b5af7
Ian Vlahovic <banddorkfht7@gmail.com>
2018-05-30 20:30:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,10 +14,9 @@ class Student
     if self.id
       self.update
     else
-      sql = <<-SQL
-        INSERT INTO students (name, grade)
-        VALUES (?, ?)
-      SQL
+      sql = "INSERT INTO students (name, grade) VALUES (?, ?)"
+      DB[:conn].execute(sql, self.name, self.grade)
+
   end
 
   def self.create_table
